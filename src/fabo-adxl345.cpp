6c385c4c5f115fb57386d680b1a8bf86beb7053a
Shimakage <shimakage@gclue.jp>
2016-04-12 16:23:31 +0900

    modified tap

--- src/fabo-adxl345.cpp
+++ src/fabo-adxl345.cpp
@@ -1,11 +1,23 @@
+/**
+ * @file  fabo-adxl345.cpp
+ * @brief fabo libtary of ADXL345
+ * @author Akira Sasaki
+ * @date 2,10, 2016
+ */
+
 #include "fabo-adxl345.h"
 
+/**
+ * @brief Search ADXL345 Device
+ * @retval true  : Found
+ * @retval false : Not Found
+ */
 bool FaBo3Axis::searchDevice()
 {
 
   byte device = 0x00;
   readI2c(ADXL345_DEVID_REG, 1, &device);
-  
+
   if(device == ADXL345_DEVICE){
     return true;
   } else{
@@ -13,6 +25,25 @@ bool FaBo3Axis::searchDevice()
   }
 }
 
+/**
+ * @brief Set Config
+ */
+void FaBo3Axis::configuration()
+{
+
+  byte conf = ADXL345_SELF_TEST_OFF;
+  conf |= ADXL345_SPI_OFF;
+  conf |= ADXL345_INT_INVERT_OFF;
+  conf |= ADXL345_FULL_RES_OFF;
+  conf |= ADXL345_JUSTIFY_OFF;
+  conf |= ADXL345_RANGE_16G;
+
+  writeI2c(ADXL345_DATA_FORMAT_REG, conf);
+}
+
+/**
+ * @brief Power ON
+ */
 void FaBo3Axis::powerOn()
 {
   byte power = ADXL345_AUTO_SLEEP_OFF;
@@ -23,33 +54,60 @@ void FaBo3Axis::powerOn()
   writeI2c(ADXL345_POWER_CTL_REG, power);
 }
 
+/**
+ * @brief Read ADXL345
+ * @param [out] *x : X-axis Data
+ * @param [out] *y : Y-axis Data
+ * @param [out] *z : Z-axis Data
+ */
 void FaBo3Axis::readXYZ(int *x, int *y, int *z)
 {
   uint8_t length = 6;
   byte axis_buff[6];
-  readI2c(0x32, length, axis_buff); //レジスターアドレス 0x32から6バイト読む
-  *x = (((int)axis_buff[1]) << 8) | axis_buff[0];   
+  readI2c(0x32, length, axis_buff);
+  *x = (((int)axis_buff[1]) << 8) | axis_buff[0];
   *y = (((int)axis_buff[3]) << 8) | axis_buff[2];
   *z = (((int)axis_buff[5]) << 8) | axis_buff[4];
 }
 
+/**
+ * @brief Read interrupts Status
+ * @return byte : interrupts Status
+ */
 byte FaBo3Axis::readIntStatus()
 {
 
   byte buff;
-  readI2c(ADXL345_INT_SOURCE_REG, 1, &buff); //レジスターアドレス 0x32から6バイト読む
-  
+  readI2c(ADXL345_INT_SOURCE_REG, 1, &buff);
+
   return buff;
 }
 
+/**
+ * @brief Enable Tap
+ */
 void FaBo3Axis::enableTap()
-{  
-  writeI2c(ADXL345_THRESH_TAP_REG, 50); // 単位 62.5mg/LBS
-  writeI2c(ADXL345_DUR_REG, 15); // 単位 1.25ms/LSB
-  writeI2c(ADXL345_LATENT_REG, 120); // 単位 1.25ms/LSB
-  writeI2c(ADXL345_WINDOW_REG, 200); // 単位 1.25ms/LSB
+{
+
+  writeI2c(ADXL345_THRESH_TAP_REG, 0x32); // 62.5mg/LBS
+  writeI2c(ADXL345_DUR_REG, 0x0f); // 1.25ms/LSB
+  writeI2c(ADXL345_LATENT_REG, 0x78); // 1.25ms/LSB
+  writeI2c(ADXL345_WINDOW_REG, 0xcb); // 1.25ms/LSB
+
+  byte int_tap = ADXL345_INT_SINGLE_TAP | ADXL345_INT_DOUBLE_TAP;
+  writeI2c(ADXL345_INT_ENABLE_REG, int_tap); // Interrupts Tap Enable
+
+  writeI2c(ADXL345_TAP_AXES_REG, ADXL345_TAP_AXES_Z_ON); // Tap Enable z axis
+
+
 }
 
+/**
+ * @brief Single Tap
+ * @param [in] value : interrupts Status
+ * @retval true  : Tap
+ * @retval false : Not Tap
+ */
 bool FaBo3Axis::isSingleTap(byte value)
 {
   if((value & 0b01000000) == 0b01000000){
@@ -59,6 +117,12 @@ bool FaBo3Axis::isSingleTap(byte value)
   }
 }
 
+/**
+ * @brief Double Tap
+ * @param [in] value : interrupts Status
+ * @retval true  : Double Tap
+ * @retval false : Not Double Tap
+ */
 bool FaBo3Axis::isDoubleTap(byte value)
 {
   if((value & 0b00100000) == 0b00100000){
@@ -68,43 +132,41 @@ bool FaBo3Axis::isDoubleTap(byte value)
   }
 }
 
-void FaBo3Axis::configuration()
+/**
+ * @brief Write I2C Data
+ * @param [in] register_addr : Write Register Address
+ * @param [in] value  : Write Data
+ */
+void FaBo3Axis::writeI2c(byte register_addr, byte value)
 {
-
-  byte conf = ADXL345_SELF_TEST_OFF;
-  conf |= ADXL345_SPI_OFF;
-  conf |= ADXL345_INT_INVERT_OFF;
-  conf |= ADXL345_FULL_RES_OFF;
-  conf |= ADXL345_JUSTIFY_OFF;
-  conf |= ADXL345_RANGE_16G;
- 
-  writeI2c(ADXL345_DATA_FORMAT_REG, conf);
+  Wire.begin();
+  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS);
+  Wire.write(register_addr);
+  Wire.write(value);
+  Wire.endTransmission();
 }
 
-// I2Cへの書き込み
-void FaBo3Axis::writeI2c(byte register_addr, byte value) {
-  Wire.begin();       // I2Cの開始
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS);  
-  Wire.write(register_addr);         
-  Wire.write(value);                 
-  Wire.endTransmission();        
-}
+/**
+ * @brief Read I2C Data
+ * @param [in] register_addr : register address
+ * @param [in] num   : Data Length
+ * @param [out] *buf : Read Data
+ */
+void FaBo3Axis::readI2c(byte register_addr, int num, byte buffer[])
+{
+  Wire.begin();
+  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS);
+  Wire.write(register_addr);
+  Wire.endTransmission();
 
-// I2Cへの読み込み
-void FaBo3Axis::readI2c(byte register_addr, int num, byte buffer[]) {
-  Wire.begin();       // I2Cの開始
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS); 
-  Wire.write(register_addr);           
-  Wire.endTransmission();         
-
-  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS); 
-  Wire.requestFrom(ADXL345_SLAVE_ADDRESS, num);   
+  Wire.beginTransmission(ADXL345_SLAVE_ADDRESS);
+  Wire.requestFrom(ADXL345_SLAVE_ADDRESS, num);
 
   int i = 0;
-  while(Wire.available())        
-  { 
-    buffer[i] = Wire.read();   
+  while(Wire.available())
+  {
+    buffer[i] = Wire.read();
     i++;
   }
-  Wire.endTransmission();         
+  Wire.endTransmission();
 }
